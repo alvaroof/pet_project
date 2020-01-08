26114595d71fc99566c7ac28d87e919cee922004
alvaroof <ortiz.fernandez.alvaro@gmail.com>
2020-01-08 23:17:38 +0100

    initial commit

---
+++ pet_project/preprocessing/load.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+def loading_my_file(path):
+    a = pd.read_csv(path)
+    return(a)
+
+
+
+
+
---
+++ pet_project/scripts/main.py
@@ -0,0 +1,9 @@
+from pet_project.preprocessing import load
+
+df = load.loading_my_file('data/wiki_movie_plots_deduped.csv')
+
+df.head()
+
+
+
+
